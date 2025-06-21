577ba42a0ecf41194ea51c26ce24839317c7e6a1
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-06-21 12:06:27 +0530

    added files

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -2,7 +2,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${process.env.REACT_APP_TMDB_TOKEN}`
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzdhYTYwMGQ4YWQzNzA1MmNmNjA5ZDBhNzc2MTU0ZiIsIm5iZiI6MTczOTI0NzYyNS4xNTYsInN1YiI6IjY3YWFkMDA5NDcwNTFjNTRiZWIwOTk1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zNvUS9ck7mBtCY65eWn38_w7TMfBTgHCBbitn1nFL-g"
   }
 };
 
@@ -16,4 +16,4 @@ export const SUPPORTED_LANGUAGES = [{identifier : "en",name : "English"},
 ];
 
 
-export const GEMINI_AI_KEY = process.env.REACT_APP_GEMINI_KEY;
+export const GEMINI_AI_KEY = "AIzaSyAIZqOLRygK9Gp48xw9qz911HBtuGojj20";
